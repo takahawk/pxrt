2f5300362dcfa81c99e35feff50f519e63ac8458
takahawk <takahawkkun@gmail.com>
2017-08-25 22:17:27 +0300

    Cleared notebook at start. Some refactoring

--- pxrt.c
+++ pxrt.c
@@ -1,23 +1,42 @@
 #include <gtk/gtk.h>
 
+typedef struct {
+	GtkWidget *root;
+	GtkNotebook *image_tabs;
+} PxrtWindow;
+
+void
+pxrt_window_init(PxrtWindow *window);
+
+
 int
 main(int argc, char **argv)
 {
-	GtkBuilder *builder;
-	GtkWidget *window;
-
+	PxrtWindow window;
+	
 	gtk_init(&argc, &argv);
+	pxrt_window_init(&window);
+	gtk_main();
+	
+	return 0;
+}
 
+void
+pxrt_window_init(PxrtWindow *window)
+{
+	GtkBuilder *builder;
+	
 	builder = gtk_builder_new_from_resource("/io/github/takahawk/pxrt/pxrt_window.glade");
-	window = GTK_WIDGET(gtk_builder_get_object(builder, "pxrt_window"));
+	window->root = GTK_WIDGET(gtk_builder_get_object(builder, "pxrt_window"));
+	window->image_tabs = GTK_NOTEBOOK(gtk_builder_get_object(builder, "image_tabs"));
+
 	gtk_builder_connect_signals(builder, NULL);
 
 	g_object_unref(builder);
 
-	gtk_widget_show(window);
-	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_widget_show(window->root);
+	g_signal_connect(window->root, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 	
-	gtk_main();
-	
-	return 0;
+	while (gtk_notebook_get_n_pages(window->image_tabs) != 0)
+		gtk_notebook_remove_page(window->image_tabs, 0);
 }
